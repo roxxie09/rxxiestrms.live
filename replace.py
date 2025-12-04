306004a1385ae22d1da5a12017594408dc566b25
turniptrainwrek <Angel.petricek@proton.me>
2025-12-03 18:03:04 -0800

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "600.shadow-ran.online" with "loveliness.wnion.co.in"
-        updated_content = content.replace("600.shadow-ran.online", "loveliness.wnion.co.in")
+        # Replace all instances of "thedaffodil.wnion.co.in" with "thedaffodill.wnion.co.in"
+        updated_content = content.replace("thedaffodil.wnion.co.in", "thedaffodill.wnion.co.in")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
