925c957a96ccd047c950be27964c0a1608594e37
turniptrainwrek <Angel.petricek@proton.me>
2025-12-14 13:11:46 -0800

    f

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "600.tanjung.eu.org" with "600.alfirdaus.eu.org"
-        updated_content = content.replace("600.tanjung.eu.org", "600.alfirdaus.eu.org")
+        # Replace all instances of "600.alfirdaus.eu.org" with "600.roxiestreams.live"
+        updated_content = content.replace("600.alfirdaus.eu.org", "600.roxiestreams.live")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
