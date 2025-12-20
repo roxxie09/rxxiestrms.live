52592f430fa016d3d0e95b7afe489e719f10f4e5
turniptrainwrek <Angel.petricek@proton.me>
2025-12-20 04:19:03 -0800

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "601.shadow-ran.online" with "601.xn--1000-ugoa0hsb9a0hb.com"
-        updated_content = content.replace("601.shadow-ran.online", "601.xn--1000-ugoa0hsb9a0hb.com")
+        # Replace all instances of "601.xn--1000-ugoa0hsb9a0hb.com" with "601.shadow-ran.online"
+        updated_content = content.replace("601.xn--1000-ugoa0hsb9a0hb.com", "601.shadow-ran.online")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
