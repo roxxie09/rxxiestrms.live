c0a39a90a0d8dd4b9b870e5f862469e5fdb0ffde
turniptrainwrek <Angel.petricek@proton.me>
2025-12-21 15:16:43 -0800

    f

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "daffodil.xn--1000-ugoa0hsb9a0hb.com" with "daffodil.shadow-ran.online"
-        updated_content = content.replace("daffodil.xn--1000-ugoa0hsb9a0hb.com", "daffodil.shadow-ran.online")
+        # Replace all instances of "601.biroslotutama.org" with "601.shadow-ran.online"
+        updated_content = content.replace("601.biroslotutama.org", "601.shadow-ran.online")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
