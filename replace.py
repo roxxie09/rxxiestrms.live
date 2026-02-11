a2c189794b42800b0dbdf55d5d1df0efc55d2360
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-02-11 12:30:37 -0800

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "main.css" with "mainn.css"
-        updated_content = content.replace("main.css", "mainn.css")
+        # Replace all instances of "domains.txt" with "domainsa.txt"
+        updated_content = content.replace("domains.txt", "domainsa.txt")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
