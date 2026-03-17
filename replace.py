681005caee1c41044eb8c3a172758600db140c53
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-03-17 03:35:45 -0700

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "serverbandel.sbs" with "lifesharper.com"
-        updated_content = content.replace("serverbandel.sbs", "lifesharper.com")
+        # Replace all instances of "domains.txt" with "domainse.txt"
+        updated_content = content.replace("domains.txt", "domainse.txt")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
