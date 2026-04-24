1455e73f723817d88a72d5a240e41f69ae490400
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-04-24 12:54:34 -0700

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "domainso" with "domainsp"
-        updated_content = content.replace("domainso", "domainsp")
+        # Replace all instances of "underupturnip" with "marllonferreira.space"
+        updated_content = content.replace("underupturnip", "marllonferreira.space")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
