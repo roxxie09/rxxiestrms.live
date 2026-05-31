43d8acd7497fd2cafa19cc80dc602708e12f4ddb
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-05-31 09:08:24 -0700

    z

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "701" with "601"
-        updated_content = content.replace("701", "601")
+        # Replace all instances of "601" with "701"
+        updated_content = content.replace("601", "701")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
