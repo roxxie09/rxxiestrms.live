84bfea1f2a3ef3538e6684043dcdfd2d6a9aa362
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-06-03 20:34:03 -0700

    a

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -9,17 +9,6 @@ function initializeCountdowns() {
 
             if (distance >= 0) {
                 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                timerElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-                timerElement.style.color = "#ff0000";
-                timerElement.style.fontWeight = "bold";
-            } else if (now <= endTime) {
-                timerElement.innerHTML = "LIVE";
-                timerElement.style.color = "red";
-                timerElement.style.fontWeight = "bold";
-            } else {
                 clearInterval(intervalId);
                 timerElement.innerHTML = "ENDED";
                 timerElement.style.color = "gray";
