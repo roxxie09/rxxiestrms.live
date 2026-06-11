be0dadef15b658a9506362ea3e865c07561da6f2
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2026-06-11 00:13:56 -0700

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "mainn" with "main2"
-        updated_content = content.replace("mainn", "main2")
+        # Replace all instances of "index1" with "index2"
+        updated_content = content.replace("index1", "index2")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
