cddb8844f929670fb9eb80bfcd961f24b5acd4f9
turniptrainwrek <Angel.petricek@proton.me>
2026-07-11 14:37:00 -0700

    a

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "domainsz46" with "domainsz47"
-        updated_content = content.replace("domainsz46", "domainsz47")
+        # Replace all instances of "domainsz47" with "domainsz48"
+        updated_content = content.replace("domainsz47", "domainsz48")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
